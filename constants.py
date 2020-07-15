2b42e0eb52676920d17190aaaee8a021736b3cb6
Ro Gallo <rgallo@medallia.com>
2020-07-14 23:40:28 -0300

    added sqare and sinusoidal responses

---
+++ constants.py
@@ -0,0 +1,26 @@
+
+##############
+# Axis units #
+##############
+
+AMPLITUDE_LABEL = 'Amplitud [m]'
+ANGULAR_FREQUENCY_LABEL = 'Frecuencia anglular ω [rad/s]'
+GAIN_LABEL = 'Modulo [dB]'
+PHASE_LABEL = 'Fase [rad]'
+TIME_LABEL = 'Tiempo [s]'
+
+
+##############
+#   Titles   #
+##############
+
+GAIN_DIAGRAM_TITLE = 'Diagrama de modulo'
+IMPULSE_RESPONSE_DIAGRAM_TITLE = 'Respuesta a la delta'
+PHASE_DIAGRAM_TITLE = 'Diagrama de fase'
+STEP_RESPONSE_DIAGRAM_TITLE = 'Respuesta al escalon'
+SQUARE_RESPONSE_F0_DIAGRAM_TITLE = "Respuesta a una señal cuadrada de frecuencia $f_0$"
+SQUARE_RESPONSE_10_TIMES_F0_DIAGRAM_TITLE = "Respuesta a una señal cuadrada de frecuencia $10*f_0$"
+SQUARE_RESPONSE_F0_OVER_10_DIAGRAM_TITLE = "Respuesta a una señal cuadrada de frecuencia $10/f_0$"
+SINE_RESPONSE_F0_DIAGRAM_TITLE = "Respuesta a una señal senoidal de frecuencia $f_0$"
+SINE_RESPONSE_GREAT_F0_TITLE = "Respuesta a una señal senoidal de frecuencia $0.1*f_0$"
+SINE_RESPONSE_LITTLE_F0_TITLE = "Respuesta a una señal senoidal de frecuencia $10000*f_0$"
--- ejercicios.py
+++ ejercicios.py
@@ -1,37 +1,14 @@
-import sympy as sym
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
 
-p = [0.8911,0,0,0,0]
+from constants import *
+
+p = [0.8911, 0, 0, 0, 0]
 q = [1, 2539, 4.686e6, 2.894e9, 2.863e12]
-
 sys = signal.TransferFunction(p, q)
-w0, mag, phase = signal.bode(sys)
-
-s = sym.symbols('s')
-H = (0.8911*s**4)/(s**4+2539*s**3+4.686e6*s**2+2.894e9*s+2.863e12)
-
-##############
-# Axis units #
-##############
-
-AMPLITUDE_LABEL = 'Amplitud [m]'
-ANGULAR_FREQUENCY_LABEL = 'Frecuencia anglular ω [rad/s]'
-GAIN_LABEL = 'Modulo [dB]'
-PHASE_LABEL = 'Fase [rad]'
-TIME_LABEL = 'Tiempo [s]'
-
-
-##############
-#   Titles   #
-##############
-
-GAIN_DIAGRAM_TITLE = 'Diagrama de modulo'
-IMPULSE_RESPONSE_DIAGRAM_TITLE = 'Respuesta a la delta'
-PHASE_DIAGRAM_TITLE = 'Diagrama de fase'
-STEP_RESPONSE_DIAGRAM_TITLE = 'Respuesta al escalon'
-
+w, mag, phase = signal.bode(sys)
+w0 = w[mag < -3][-1]
 
 #################
 # Bode Diagrams #
@@ -39,7 +16,7 @@ STEP_RESPONSE_DIAGRAM_TITLE = 'Respuesta al escalon'
 
 # Gain diagram
 plt.figure()
-plt.semilogx(w0, mag)
+plt.semilogx(w, mag)
 plt.xlabel(ANGULAR_FREQUENCY_LABEL)
 plt.ylabel(GAIN_LABEL)
 plt.title(GAIN_DIAGRAM_TITLE)
@@ -48,7 +25,7 @@ plt.show()
 
 # Phase diagram
 plt.figure()
-plt.semilogx(w0, phase)
+plt.semilogx(w, phase)
 plt.xlabel(ANGULAR_FREQUENCY_LABEL)
 plt.ylabel(PHASE_LABEL)
 plt.title(PHASE_DIAGRAM_TITLE)
@@ -79,22 +56,98 @@ plt.title(IMPULSE_RESPONSE_DIAGRAM_TITLE)
 plt.grid()
 plt.show()
 
-f0 = w0/(2*np.pi)
-ten_times_f0 = 10*f0
-a_tenth_f0 = f0/10
+t = np.linspace(0, 1, 1000000, endpoint=False)
 
-t = np.linspace(0, 1, 100, endpoint=False) # sampling frequency of 100Hz for my waveforms
+sq_resp_f0 = signal.lsim(sys, signal.square(w0 * t), t)[1]
+sq_resp_10_times_f0 = signal.lsim(sys, signal.square(10 * w0 * t), t)[1]
+sq_resp_f0_over_10 = signal.lsim(sys, signal.square(w0 / 10 * t), t)[1]
 
-# Squared signal with frequency f0
-plt.plot(t, signal.square(2 * np.pi * f0 * t))
+# Response to squared signal with frequency f0
+plt.plot(t, sq_resp_f0)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SQUARE_RESPONSE_F0_DIAGRAM_TITLE)
 plt.ylim(-2, 2)
+plt.xlim(0, 0.04)
+plt.grid()
+plt.show()
 
-# Squared signal with frequency 10*f0
-plt.plot(t, signal.square(2 * np.pi * ten_times_f0 * t))
+# Response to squared signal with frequency 10*f0
+plt.plot(t, sq_resp_10_times_f0)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SQUARE_RESPONSE_10_TIMES_F0_DIAGRAM_TITLE)
 plt.ylim(-2, 2)
+plt.xlim(0, 0.01)
+plt.grid()
+plt.show()
 
-# Squared signal with frequency f0/10
-plt.plot(t, signal.square(2 * np.pi * a_tenth_f0 * t))
+# Response to squared signal with frequency f0/10
+plt.plot(t, sq_resp_f0_over_10)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SQUARE_RESPONSE_F0_OVER_10_DIAGRAM_TITLE)
 plt.ylim(-2, 2)
+plt.xlim(0, 0.2)
+plt.grid()
+plt.show()
+
+f = w0 / (2 * np.pi)
+little_f = f * 0.1
+great_f = f * 1000
+
+sin_little_f0 = np.sin(w0 * 0.1 * t)
+sin_f0 = np.sin(w0 * t)
+sin_great_f0 = np.sin(w0 * 10000 * t)
+
+sin_resp_little_f0 = signal.lsim(sys, sin_little_f0, t)[1]
+sin_resp_f0 = signal.lsim(sys, sin_f0, t)[1]
+sin_resp_great_f0 = signal.lsim(sys, sin_great_f0, t)[1]
+
+# Response to sinusoidal signal with frequency 0.1*f0
+plt.plot(t, sin_little_f0)
+plt.plot(t, sin_resp_little_f0)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SINE_RESPONSE_LITTLE_F0_TITLE)
+plt.ylim(-2, 2)
+plt.xlim(0.5, 0.501)
+plt.grid()
+plt.show()
+
+# Response to sinusoidal signal with frequency f0
+plt.plot(t, sin_f0)
+plt.plot(t, sin_resp_f0)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SINE_RESPONSE_F0_DIAGRAM_TITLE)
+plt.ylim(-2, 2)
+plt.xlim(0.5, 0.04)
+plt.grid()
+plt.show()
+
+# Response to sinusoidal signal with frequency 10000*f0
+plot_sinusoidal_response(sys, great_f, t, SINE_RESPONSE_GREAT_F0_TITLE, 0.01)
+plt.plot(t, sin_great_f0)
+plt.plot(t, sin_resp_great_f0)
+plt.xlabel(TIME_LABEL)
+plt.ylabel(AMPLITUDE_LABEL)
+plt.title(SINE_RESPONSE_GREAT_F0_TITLE)
+plt.ylim(-2, 2)
+plt.xlim(0.5, 0.01)
+plt.grid()
+plt.show()
 
 
+def plot_sinusoidal_response(system, f, x, title, xlim_sup):
+    sin = np.sin(2 * np.pi * f * x)
+    sin_resp = signal.lsim(system, sin, x)[1]
+    plt.plot(t, sin)
+    plt.plot(t, sin_resp)
+    plt.xlabel(TIME_LABEL)
+    plt.ylabel(AMPLITUDE_LABEL)
+    plt.title(title)
+    plt.ylim(-2, 2)
+    plt.xlim(0.5, xlim_sup)
+    plt.grid()
+    plt.show()
